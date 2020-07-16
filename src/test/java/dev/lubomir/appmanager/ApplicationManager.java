0c9b55f06c8ca8f4482048a7e03afbee3d579917
Lubomir <68271388+lubomirpapaya@users.noreply.github.com>
2020-07-16 22:13:09 +0300

    added log4j dependencies

--- src/test/java/dev/lubomir/appmanager/ApplicationManager.java
+++ src/test/java/dev/lubomir/appmanager/ApplicationManager.java
@@ -1,16 +1,21 @@
 package dev.lubomir.appmanager;
 
 import dev.lubomir.util.Constants;
+import org.apache.logging.log4j.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.ITestContext;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
   public ChromeDriver driver;
+  protected Logger log;
   String url = "https://lubomir.dev";
   String googleUrl = Constants.SERVER_URL;
 
-  public void init() {
+  public void init(ITestContext ctx) {
+    String testName = ctx.getCurrentXmlTest().getName();
+    log = LogManager.getLogger(testName);
     //create driver
     System.setProperty("webdriver.chrome.driver", "/Users/mac/SeleniumPajeObjectFromScratch/lubomir.dev/src/main/resources/chromedriver");
 
--- src/test/java/dev/lubomir/tests/TestBase.java
+++ src/test/java/dev/lubomir/tests/TestBase.java
@@ -1,15 +1,18 @@
 package dev.lubomir.tests;
 
 import dev.lubomir.appmanager.ApplicationManager;
+import org.apache.logging.log4j.core.Logger;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
   protected final ApplicationManager applicationManager = new ApplicationManager();
+  protected Logger log;
 
   @BeforeMethod
-  public void setup() { applicationManager.init();
+  public void setup(ITestContext ctx) { applicationManager.init(ctx);
   }
 
   @AfterMethod
